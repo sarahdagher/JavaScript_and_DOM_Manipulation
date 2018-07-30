2edd38f1d2335a2fef764a0496c0a6d40e018018
Sarah D <sarahdagher93@gmail.com>
2018-07-30 18:18:54 -0500

    fix data

---
+++ australia.js
@@ -0,0 +1,11 @@
+var australia = 
+    {
+      "ACT": "Australian Capital Territory",
+      "NSW": "New South Wales",
+      "NT": "Northern Territory",
+      "QLD": "Queensland",
+      "SA": "South Australia",
+      "TAS": "Tasmania",
+      "VIC": "Victoria",
+      "WA": "Western Australia"
+    }
